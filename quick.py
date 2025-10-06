bfe957107a2f3b972723a32da8db6f94a9bbc308
tiw1412 <tim10mish@gmail.com>
2025-10-06 20:02:40 +0300

    1wd

--- quick.py
+++ quick.py
@@ -3,8 +3,9 @@ l = []
 bg = []
 lw = []
 dey = 0
+z = 0
 count = int(input('Введите количество чисел: '))
-op = count - 1 #r.randint(0, count - 1)
+#r.randint(0, count - 1)
 for i in range (count):
     qn = int(input('Введите число: '))
     l.append(qn)
@@ -13,32 +14,60 @@ ixr = 0
 hassorted = False
 
 def rp(op, std, ixr, dey):
-    l[op - 1], l[std] = l[std], l[op - 1]
-    std += 1
     if l[std] > l[op]:
             ixr = std
             while l[ixr] < l[op]:
                 l[ixr], l[ixr + 1] = l[ixr + 1], l[ixr]
                 ixr += 1
             print(dey , l)
-    if l[std] < l[op]:
+    if l[std] <= l[op]:
         hassorted = True
 
-
+op = count - 1
 print(op)
 print(l)
 
 
-while hassorted != True:
-#for i in range(count ** 2):
-    if l[op - 1] < l[op]:
-        lw.append(l[op - 1])
-        dey = 'Пр'
-        rp(op, std, ixr, dey)
-    if l[op - 1] >= l[op]:
-        bg.append(l[op - 1])
-        dey = 'Пк. '
-        rp(op, std, ixr, dey)
+
+#while hassorted != True:
+for i in range(count):
+    if l[op - z] < l[op]:
+        lw.append(l[op - z])
+        z += 1
+        if op - z == -1:
+            break
+            # l[op - 1], l[std] = l[std], l[op - 1]
+            # std += 1
+            # dey = 'Пр'
+            # rp(op, std, ixr, dey)
+    if l[op - z] >= l[op]:
+        bg.append(l[op - z])
+        z += 1
+        if op - z == -1:
+            break
+            # l[op - 1], l[std] = l[std], l[op - 1]
+            # dey = 'Пк. '
+            # rp(op, std, ixr, dey)
+
+
+
+
+for i in range(count - (len(lw) + len(lw) - 1)):
+        
+# op = len(lw)
+# std = 0
+# while std != op:
+#     lw[op - 1], lw[std] = lw[std], lw[op - 1]
+#     std += 1
+
+# op = len(bg)
+# std = 0
+# while std != op:
+#     bg[op - 1], bg[std] = bg[std], bg[op - 1]
+#     std += 1
+
+o = lw + bg
+
 print(l)
 print(hassorted)
 print(op)
@@ -47,3 +76,4 @@ print(std)
 print(l[std])
 print(lw)
 print(bg)
+print(o)
