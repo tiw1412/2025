bd1cc3f4446b7732314c06cc611f7de613b64513
tiw1412 <tim10mish@gmail.com>
2025-10-30 18:39:26 +0300

    Add files via upload

--- zmay.py
+++ zmay.py
@@ -1,12 +1,20 @@
 class field:
-    def __init__(self, height, width):
+    def __init__(self, height, width, mainsymb, upgs, lrgs, gen = []):
         self.height = height
         self.width = width
+        self.mainsymb = mainsymb
+        self.upgs = upgs
+        self.lrgs = lrgs
+        self.gen = gen
+    def pr_cl(self):
+        astr = []
+        for i in range(self.width):
+            astr.append(self.mainsymb)
+        for i in range(self.height):
+            print(str(astr))
+        self.gen.append(astr)
+        return(self.gen)
 
-
-
-mainmap = field(16, 16)
-print('☪' * (mainmap.width + 2))
-for i in range(mainmap.height):
-    print('☪' + '✡' * mainmap.width + '☪')
-print('☪' * (mainmap.width + 2))
+mainmap = field(16, 16, '✡', '☪', '☪')
+mainmap.pr_cl()
+print(mainmap.pr_cl())
